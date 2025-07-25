81dacf7198a701210539a795ac00fd9198209fcd
KeiraDiaz <nkeiradiaz@gmail.com>
2025-07-25 17:48:30 +0700

    DEBUG: Added some modifications

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <WiFiMulti.h>
 
-#define WIFI_SSID "Hah loser"
-#define WIFI_PASSWORD "imagine thinking i would just post my password"
+#define WIFI_SSID "yuh uh"
+#define WIFI_PASSWORD "nuh uh"
 
 WiFiMulti wifiMulti;
 
@@ -11,12 +11,25 @@ void setup() {
   pinMode(2, OUTPUT);
 
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
   while (wifiMulti.run() != WL_CONNECTED){
-    delay(100);
+    Serial.print(".");
+    delay(500);
   }
 
-  Serial.println('Connected');
+  Serial.println("\nConnected to WiFi!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  IPAddress ip;
+  if (WiFi.hostByName("google.com", ip)) {
+    Serial.print("google.com resolves to: ");
+    Serial.println(ip);
+  } else {
+    Serial.println("DNS resolution failed.");
+  }
 }
+
 
 void loop() {
   digitalWrite(2, WiFi.status() == WL_CONNECTED);
